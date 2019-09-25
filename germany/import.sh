834c13a885a2ef48eb06c7588aa883d865a2075f
Steffen Volkmann <volkmann@devpool.net>
2019-09-25 09:03:09 +0200

    update sample scripts

--- germany/import.sh
+++ germany/import.sh
@@ -4,14 +4,13 @@ docker run \
 	--name openstreetmap-tile-server-germany \
   --rm=false \
 	-e THREADS=8 \
-	-e NCACHE=4096 \
 	-e UPDATES=enabled \
 	-e AUTOVACUUM=off \
-	-e OSM2PGSQL_EXTRA_ARGS="--flat-nodes /nodes/flat_nodes.bin" \
+	-e OSM2PGSQL_EXTRA_ARGS="--flat-nodes /nodes/flat_nodes.bin -C 4096" \
 	-v $PWD/volumes/download/germany-latest.osm.pbf:/data.osm.pbf \
 	-v $PWD/volumes/download/germany.poly:/data.poly \
 	-v openstreetmap-data-germany-latest:/var/lib/postgresql/10/main \
 	-v openstreetmap-rendered-tiles-germany-latest:/var/lib/mod_tile \
-	-v openstreetmap-flat-germany-latest:/flat \
+	-v openstreetmap-flat-germany-latest:/nodes \
 	openstreetmap-tile-server-germany  \
 	import
--- germany/start.sh
+++ germany/start.sh
@@ -6,7 +6,6 @@ docker run \
 		-e THREADS=8 \
 		-e OSM2PGSQL_EXTRA_ARGS="--flat-nodes /nodes/flat_nodes.bin" \
 		-e UPDATES=enabled \
-		-e NCACHE=4096 \
 		--publish 8000:80 \
 		--shm-size=3G \
 		--restart unless-stopped \
@@ -15,6 +14,6 @@ docker run \
 		-v /media/data/openstreetmap-tile-server/volumes/download/germany-latest.osm.pbf:/data.osm.pbf \
 		-v openstreetmap-data-germany-latest:/var/lib/postgresql/10/main \
 		-v openstreetmap-rendered-tiles-germany-latest:/var/lib/mod_tile \
-		-v openstreetmap-flat-germany-latest:/flat \
+		-v openstreetmap-flat-germany-latest:/nodes \
 		openstreetmap-tile-server-germany  \
 		run
