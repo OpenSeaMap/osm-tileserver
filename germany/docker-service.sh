c14767c7a030fc1c80e2fac4ac4d8d9c38fddf7b
Steffen Volkmann <volkmann@devpool.net>
2019-09-30 12:41:07 +0200

    update scripts for germany

---
+++ germany/docker-service.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# start and stops the osm tile server
+
+set -x
+
+IMAGE_NAME="openstreetmap-tile-server-germany"
+
+
+case "$1" in
+  build)
+		docker build -t $IMAGE_NAME ./src
+	;;
+  connect)
+		docker exec -i -t $IMAGE_NAME /bin/bash
+	;;
+  log)
+		docker logs -f $IMAGE_NAME
+	;;
+  *)
+	echo "Usage: docker.service.sh {start|stop|restart|build|connect|import|log}" >&2
+	exit 1
+	;;
+esac
+
+exit 0
