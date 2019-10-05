0e6271fd8ad86481581dbc759ccfabba61c4ddf7
Steffen Volkmann <volkmann@devpool.net>
2019-10-05 12:12:50 +0200

    update planet download file to planet-190930.osm.pbf

--- planet/download.sh
+++ planet/download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # import planet file
 
-wget -c https://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/pbf/planet-190923.osm.pbf -P ./volumes/download/
-wget -c https://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/pbf/planet-190923.osm.pbf.md5 -P ./volumes/download/
+wget -c https://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/pbf/planet-190930.osm.pbf -P ./volumes/download/
+wget -c https://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/pbf/planet-190930.osm.pbf.md5 -P ./volumes/download/
